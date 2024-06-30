3cab565541d472e6c1ba6a9f4f7363e289f41ba4
Md. Talal Wasim <md.talalwasim@gmail.com>
2024-07-01 00:13:04 +0600

    Program: Insertion Operation on Array

---
+++ PracticeDSA/src/practiceDSAapc/InsertionOperationArrayProgram.java
@@ -0,0 +1,28 @@
+package practiceDSAapc;
+
+import java.util.Arrays;
+
+public class InsertionOperationArrayProgram {
+
+	public static void main(String[] args) {
+		
+		//creating an int type array of size 5.
+		int myArray[] = new int[5];
+		System.out.println("Before Insert Element into Array:: Array is Empty:");
+		for(int i=0; i<5; i++) {
+			//print empty array
+			System.out.println("myArray ["+ i +"] = "+myArray[i]);
+		}
+		System.out.println("Going to Insert Elements into myArray:");
+		for(int i=0; i<5; i++) {
+			myArray[i]= i+1;
+			System.out.println("myArray ["+i+"] = "+myArray[i]);
+		}
+	
+		System.out.println("Final Array Look Like :"+Arrays.toString(myArray));
+		
+		
+
+	}
+
+}
