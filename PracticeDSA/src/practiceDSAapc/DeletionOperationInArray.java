034b09f072a2bac8781e87c576a63c1707c765cb
Md. Talal Wasim <md.talalwasim@gmail.com>
2024-07-01 17:07:01 +0600

    Array - Deletion Operation

---
+++ PracticeDSA/src/practiceDSAapc/DeletionOperationInArray.java
@@ -0,0 +1,31 @@
+package practiceDSAapc;
+
+import java.util.Iterator;
+
+public class DeletionOperationInArray {
+
+	public static void main(String[] args) {
+
+		// int myArray[] = {1,2,3,4,5};
+		int myArray[] = new int[3];
+		int n = myArray.length;
+
+		System.out.println("Before Deletion Array Is:");
+		for (int i = 0; i < n; i++) {
+			myArray[i] = i + 1;
+			System.out.println("myArray [" + i + "] = " + myArray[i]);
+		}
+		
+		for (int i = 1; i < n - 1; i++) {
+			myArray[i] = myArray[i + 1];
+			n = n - 1;
+		}
+		
+		System.out.println("After Deletion Array Is:");
+		for (int i = 0; i < n; i++) {
+			System.out.println("myArray [" + i + "] = " + myArray[i]);
+		}
+
+	}
+
+}
