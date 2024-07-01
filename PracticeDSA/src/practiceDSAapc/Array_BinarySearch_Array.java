c90def6e2cb10e351067880e136f13dbf3699bba
Md. Talal Wasim <md.talalwasim@gmail.com>
2024-07-01 22:57:19 +0600

    Binary Search program.

---
+++ PracticeDSA/src/practiceDSAapc/Array_BinarySearch_Array.java
@@ -0,0 +1,41 @@
+package practiceDSAapc;
+
+public class Array_BinarySearch_Array {
+
+	public static void main(String[] args) {
+		System.out.println("Binary Search Program...");
+		int myArray[] = { 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30 };
+
+		int key = 28;
+
+		int result = binarySearchPerform(myArray, key);
+
+		if (result == -1) {
+			System.out.println(key + " Not Found");
+		} else {
+			System.out.println("Key: " + key + ", Found at: " + result);
+		}
+
+	}
+
+	private static int binarySearchPerform(int[] myArray, int key) {
+		int start = 0;
+		int end = myArray.length - 1;
+
+		while (start <= end) {
+			int mid = (start + end) / 2;
+
+			if (myArray[mid] == key) {
+				return mid;
+			}
+			if (mid < key) {
+				start = mid + 1;
+			} else {
+				end = mid - 1;
+			}
+		}
+		return -1;
+
+	}
+
+}
