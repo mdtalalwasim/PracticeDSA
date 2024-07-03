919db7c77128555c94cb61240573287fca729dc2
Md. Talal Wasim <md.talalwasim@gmail.com>
2024-07-03 23:20:26 +0600

    Find Largest Element: Using Java Collections max() method:

---
+++ PracticeDSA/src/practiceDSAapc/ArrayProgram/FindLargestElementInArray_Using_Java_Collections.java
@@ -0,0 +1,24 @@
+package practiceDSAapc.ArrayProgram;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class FindLargestElementInArray_Using_Java_Collections {
+
+	public static void main(String[] args) {
+		System.out.println("Find Largest Element: Using Java Collections max() method:");
+
+		// Declaring array
+		int myArray[] = { 10, 20, 30, 40, 50, 60, 500 };
+
+		List<Integer> list = new ArrayList<Integer>();
+
+		for (int i = 0; i < myArray.length; i++) {
+			list.add(myArray[i]);
+		}
+		System.out.println("Largest Element : " + Collections.max(list));
+
+	}
+
+}
