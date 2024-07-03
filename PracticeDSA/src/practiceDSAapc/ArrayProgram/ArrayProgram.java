21a86ed258066794406cd892b5dc86dc97e4da14
Md. Talal Wasim <md.talalwasim@gmail.com>
2024-07-03 21:24:03 +0600

    Welcome to DSA Practice Array Program

--- PracticeDSA/src/practiceDSAapc/ArrayProgram/ArrayProgram.java
+++ PracticeDSA/src/practiceDSAapc/ArrayProgram/ArrayProgram.java
@@ -3,7 +3,7 @@ package practiceDSAapc.ArrayProgram;
 public class ArrayProgram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Welcome to DSA Practice Array Program");
 
 	}
 
