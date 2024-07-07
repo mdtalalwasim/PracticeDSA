a13a0f6893dd98eeaa9d261bb612f31e24e5dde9
Md. Talal Wasim <md.talalwasim@gmail.com>
2024-07-07 23:29:39 +0600

    Array_Find_Largest_Two_Numbers

---
+++ PracticeDSA/src/practiceDSAapc/ArrayProgram/Array_Find_Largest_Two_Numbers.java
@@ -0,0 +1,13 @@
+package practiceDSAapc.ArrayProgram;
+
+public class Array_Find_Largest_Two_Numbers {
+
+	public static void main(String[] args) {
+
+		int myArray[] = { 10, 20, 30, 40, 50, 60, 70 };
+		
+		
+
+	}
+
+}
