bb67fbe6efab5e8e785763c6e884621e3219fac9
Md. Talal Wasim <md.talalwasim@gmail.com>
2024-07-09 00:13:28 +0600

    Patterns Problem:

---
+++ PracticeDSA/src/practiceDSAapc/Patterns/Pattern_02.java
@@ -0,0 +1,51 @@
+package practiceDSAapc.Patterns;
+
+import java.util.Scanner;
+
+public class Pattern_02 {
+
+/**
+Pattern Output:
+		 
+For this patter: insert value : 4		 
+ * 
+ * *
+ * * *
+ * * * *
+	
+**/
+	
+	//with function:
+	static void printPattern1(int n) {
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+	
+	public static void main(String[] args) {
+		//without function: Inside only main function:
+		
+		
+		//Scanner sc = new Scanner(System.in);
+		//int n = sc.nextInt();
+		/*for(int i=0; i<n; i++) {
+			for(int j =0; j<n; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}*/
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Insert value for N:");
+		int n = sc.nextInt();
+		
+		//call function:
+		printPattern1(n);
+	
+	}
+
+}
